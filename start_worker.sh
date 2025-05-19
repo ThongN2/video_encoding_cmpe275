9b625e337732500fd3649e5e90ef4496230cba8f
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2025-05-18 18:33:51 -0700

    prune workers once disconnected & removed ignore_errors

--- start_worker.sh
+++ start_worker.sh
@@ -1,3 +1,4 @@
+# Remove the backup-servers flag to run without backups
 python node.py --role worker --host localhost --port 50061 --master localhost:50051 --backup-servers localhost:50061
 python node.py --role worker --host localhost --port 50062 --master localhost:50051 --backup-servers localhost:50061
 python node.py --role worker --host localhost --port 50063 --master localhost:50051 --backup-servers localhost:50061
